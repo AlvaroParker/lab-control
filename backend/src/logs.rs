74fb4153b46899cb5ccc1897d0710fd29e13d31e
Parker <aparkerdf@protonmail.com>
2023-11-13 19:56:53 -0300

    Clippy

--- backend/src/logs.rs
+++ backend/src/logs.rs
@@ -4,5 +4,5 @@ use axum::{http::Request, middleware::Next, response::Response};
 pub async fn log_layer<B>(request: Request<B>, next: Next<B>) -> Result<Response, ()> {
     tracing::info!("{} Request to {}", request.method(), request.uri());
     tracing::info!("{:#?}", request.headers());
-    return Ok(next.run(request).await);
+    Ok(next.run(request).await)
 }
