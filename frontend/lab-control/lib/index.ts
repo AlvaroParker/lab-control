e5559a6269f83c5c7c8833227f00bbf3de92b1de
Parker <aparkerdf@protonmail.com>
2024-04-27 19:22:35 -0400

    ESLint

--- frontend/lab-control/lib/index.ts
+++ frontend/lab-control/lib/index.ts
@@ -1,9 +1,9 @@
-import AuthService from "./auth.service";
-import DeleteService from "./delete.service";
-import GetService from "./get.service";
-import PostService from "./post.service";
-import ServiceTypes from './types'
-import { Status } from "./types";
+import * as AuthService from "./auth.service";
+import * as DeleteService from "./delete.service";
+import * as GetService from "./get.service";
+import * as PostService from "./post.service";
+import * as ServiceTypes from './types';
+import { Status } from './types';
 
 export let IP = "192.168.68.115"
 export let PORT = "8080"
@@ -37,11 +37,63 @@ export function setIPandPort(ip: string, port: string) {
   PORT = port;
 }
 
+
 export {
+  /**
+   * This export contains all the functions that handle the authentication of the user.
+   * Possible responses (`Status`) are:
+   * - `Status.OK` (200)
+   * - `Status.BAD_REQUEST` (400)
+   * - `Status.UNAUTHORIZED` (401)
+   * - `Status.NOT_FOUND` (404)
+   * - `Status.INTERNAL_SERVER_ERROR` (500)
+   * 
+   * You should try to handle the `Status` of the request to give feedback to the user.
+   */
   AuthService,
+
+
+  /**
+   * This export contains all the functions that make DELETE requests to the server.
+   * Possible responses (`Status`) are:
+   * - `Status.OK` (200)
+   * - `Status.BAD_REQUEST` (400)
+   * - `Status.UNAUTHORIZED` (401)
+   * - `Status.NOT_FOUND` (404)
+   * - `Status.INTERNAL_SERVER_ERROR` (500)
+   * 
+   * You should try to handle the `Status` of the request to give feedback to the user.
+   */
   DeleteService,
+
+  /**
+   * This export contains all the functions that make GET requests to the server.
+   * Possible responses (`Status`) are:
+   * - `Status.OK` (200)
+   * - `Status.BAD_REQUEST` (400)
+   * - `Status.UNAUTHORIZED` (401)
+   * - `Status.NOT_FOUND` (404)
+   * - `Status.INTERNAL_SERVER_ERROR` (500)
+   * 
+   * You should try to handle the `Status` of the request to give feedback to the user.
+   */
   GetService,
+
+
+  /**
+   * This export contains all the functions that make POST requests to the server.
+   * Possible responses (`Status`) are:
+   * - `Status.OK` (200)
+   * - `Status.BAD_REQUEST` (400)
+   * - `Status.UNAUTHORIZED` (401)
+   * - `Status.NOT_FOUND` (404)
+   * - `Status.CONFLICT` (409)
+   * - `Status.INTERNAL_SERVER_ERROR` (500)
+   * 
+   * You should try to handle the `Status` of the request to give feedback to the user.
+   */
   PostService,
+
   ServiceTypes,
   Status
 }
--- frontend/lab-control/lib/types.ts
+++ frontend/lab-control/lib/types.ts
@@ -10,86 +10,75 @@ export enum Status {
     INTERNAL_SERVER_ERROR = 500,
     UNKNOWN = 0,
 }
-namespace ServiceTypes {
-    export interface Usuario {
-        nombre: string;
-        apellido_1: string;
-        apellido_2: string;
-        rut: string;
-        print_path: string;
-        correo_uai: string;
-        rol: string;
-        last_registro: Registro | null;
-    }
-    export interface Motivo {
-        id: number;
-        motivo: string;
-    }
-    export interface Rol {
-        id: number;
-        rol: string;
-    }
-    export interface Registro {
-        id: number;
-        nombre: string;
-        apellido_1: string;
-        apellido_2: string;
-        correo_uai: string;
-        rut: string;
-        fecha: string;
-        salida: boolean;
-        rol: string;
-        motivo: string;
-    }
-    export interface Admin {
-        nombre: string;
-        apellido_1: string;
-        apellido_2: string;
-        email: string;
-        token: string;
-    }
-    export interface Registro {
-        id: number;
-        fecha: string;
-        motivo: string;
-        rut: string;
-        salida: boolean;
-    }
-    export interface AdminRegistro {
-        nombre: string,
-        apellido_1: string,
-        apellido_2: string,
-        email: string,
-        pswd: string
-    }
-    export interface AdminGeneric {
-        nombre: string,
-        apellido_1: string,
-        apellido_2: string,
-        email: string
-    }
-    // Check if object has all the properties of Usuario (except for print_path)
-    export const isUsuario = (input: any): input is Usuario => {
-        const schema: Record<keyof Usuario, string> = {
-            nombre: 'string',
-            apellido_1: 'string',
-            apellido_2: 'string',
-            rut: 'string',
-            print_path: 'string',
-            correo_uai: 'string',
-            rol: 'string',
-            last_registro: 'string',
-        };
-        const missingProperties = Object.keys(schema)
-            .filter(
-                (key) =>
-                    (input[key] === undefined || (input[key] as string).trim().length === 0) &&
-                    key !== 'print_path' &&
-                    key !== 'last_registro'
-            )
-            .map((key) => key as keyof Usuario);
-
-        return missingProperties.length === 0;
-    };
+export interface Usuario {
+    nombre: string;
+    apellido_1: string;
+    apellido_2: string;
+    rut: string;
+    print_path: string;
+    correo_uai: string;
+    rol: string;
+    last_registro: Registro | null;
 }
-export default ServiceTypes;
+export interface Motivo {
+    id: number;
+    motivo: string;
+}
+export interface Rol {
+    id: number;
+    rol: string;
+}
+export interface Registro {
+    id: number;
+    nombre: string;
+    apellido_1: string;
+    apellido_2: string;
+    correo_uai: string;
+    rut: string;
+    fecha: string;
+    salida: boolean;
+    rol: string;
+    motivo: string;
+}
+export interface Admin {
+    nombre: string;
+    apellido_1: string;
+    apellido_2: string;
+    email: string;
+    token: string;
+}
+export interface Registro {
+    id: number;
+    fecha: string;
+    motivo: string;
+    rut: string;
+    salida: boolean;
+}
+export interface AdminRegistro {
+    nombre: string,
+    apellido_1: string,
+    apellido_2: string,
+    email: string,
+    pswd: string
+}
+export interface AdminGeneric {
+    nombre: string,
+    apellido_1: string,
+    apellido_2: string,
+    email: string
+}
+// Check if object has all the properties of Usuario (except for print_path)
+export const isUsuario = (input: Usuario): boolean=> {
+    if (
+        input &&
+        typeof input.nombre === 'string' &&
+        typeof input.apellido_1 === 'string' &&
+        typeof input.apellido_2 === 'string' &&
+        typeof input.rut === 'string' &&
+        typeof input.correo_uai === 'string' &&
+        typeof input.rol === 'string' &&
+        input.nombre && input.apellido_1 && input.apellido_2 && input.rut && input.correo_uai && input.rol
+    ) return true
+    
+    return false
+};
